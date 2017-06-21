44d9c31822e5604cdb8da2c6af92548ec6a0acf0
iMocco <taoxin167@gmail.com>
2017-06-21 14:35:40 +0800

    修改首页样式布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,7 +3,8 @@
 const AV = require('./../../utils/libs/av-weapp-min.js')
 Page({
   data: {
-    list:[]
+    list:{},
+    dates:[]
   },
   onPullDownRefresh: function () {
     this.onLoad();
@@ -16,15 +17,35 @@ Page({
     var query = new AV.Query('AccountBook');
     query.descending('createdAt');
     query.find().then(function(results) {
-      that.setData({
-        list:results
-      })
       wx.stopPullDownRefresh({
         complete: function (res) {
-          console.log(res, new Date())
+          that.transData(results);
         }
       })
     }, function(error) {});
+  },
+  transData(data){
+    var dates = [];
+    var keys = {};
+    var values = {};
+    for (var i = 0; i < data.length; i++) {
+      var result = data[i]._serverData;
+      var key =  result.date
+      var value = keys[key]
+      if(!value){
+        keys[key]=key;
+        dates.push(key);
+        values[key] = [];
+        values[key].push(result);
+      }else{
+        values[key].push(result);
+      }
+    }
+    this.setData({
+      list:values,
+      dates:dates
+    })
+    console.log(this.data.dates);
   }
 })
 
